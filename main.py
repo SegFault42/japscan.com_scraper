98b63244efb8ff473e14fe6513d88421b305e8d6
SegFault42 <rabougue@student.42.fr>
2017-08-06 05:54:01 +0200

    get chapter ok

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 import urllib2
 import urllib
 import sys
@@ -12,13 +14,19 @@ hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML,
        'Accept-Language': 'en-US,en;q=0.8',
        'Connection': 'keep-alive'}
 
+def get_index_of(value, in_list):
+    for index, valeur in enumerate(in_list):
+        if valeur == value:
+            return index
+    return -1
+
 def open_html(website):
     req = urllib2.Request(website, headers=hdr)
     response = urllib2.urlopen(req)
     data = response.read()
     return data
 
-def get_manga_and_chapter_name(website):
+def get_manga_name(website):
     soup = BeautifulSoup(website)
     for link in soup.findAll("select"):
         if (link.get("data-nom") != "None"):
@@ -26,9 +34,22 @@ def get_manga_and_chapter_name(website):
             break
     return manga_name
 
+def get_chapter_name(website):
+    soup = BeautifulSoup(website)
+    td = soup.findAll("td")
+
+    for index, valeur in enumerate(td):
+        if str(valeur) == "<td>Titre:</td>":
+            chapter_name = td[index + 1]
+            break
+    chapter_name = re.findall('>(.*)<', str(chapter_name))
+    return chapter_name
+
 def main():
     website = open_html(sys.argv[1])
-    print get_manga_and_chapter_name(website)
+    manga_name = get_manga_name(website)
+    chapter_name = get_chapter_name(website)
+    print chapter_name[0]
 
 if __name__ == '__main__':
     main()
